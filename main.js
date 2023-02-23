2329cf6bb2583c5cf840f0f030a16f92a9a7907e
Saleh Ahmad Shakil <shakil493888@gmail.com>
2023-02-23 10:54:31 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// ♫ ♫ IT'S THE FINAL COUNTDOWN ♫ ♫ //
+
+var endingDate = new Date("Nov 14, 2018 13:37:00").getTime();
+var x = setInterval(function () {
+	var current = new Date().getTime();
+	var remainingTime = endingDate - current;
+
+	var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+	var hours = Math.floor(
+		(remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	);
+	var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+	document.getElementById("countdown").innerHTML =
+		days +
+		"<span>D</span> " +
+		hours +
+		"<span>H</span> " +
+		minutes +
+		"<span>M</span> " +
+		seconds +
+		"<span>S</span> ";
+}, 1000);
